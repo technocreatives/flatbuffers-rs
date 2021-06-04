f8f936c147332ee767a9220dc1d3db46876daf93
Brendan Molloy <brendan@technocreatives.com>
2021-06-04 11:22:32 +0200

    Add Scalar and SingleValue

--- fbs-parser/src/lib.rs
+++ fbs-parser/src/lib.rs
@@ -4,5 +4,5 @@ mod lexer;
 pub use lexer::{
     ident, parse_path, Attribute, Enum, EnumValue, Error, Field, FileExtension, FileIdentifier,
     Ident, Include, Literal, Metadata, Namespace, Primitive, RootType, Schema, SchemaType, Struct,
-    Table, Type, Union, UnionValue,
+    Table, Type, Union, UnionValue, Scalar, SingleValue,
 };
